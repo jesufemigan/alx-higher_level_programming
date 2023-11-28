9d611413ee6402a9cb1d642f95e4b0915c8c6224
Jesufemi <oladapofemmy@gmail.com>
2023-11-28 16:38:20 +0100

    fix 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,12 +12,12 @@ int check_cycle(listint_t *list)
 {
 	listint_t *slow = list, *fast = list;
 
-	while (slow && fast->next)
+	while (slow && fast && fast->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
 		if (slow == fast)
 			return (1);
-		return (0);
 	}
+	return (0);
 }
