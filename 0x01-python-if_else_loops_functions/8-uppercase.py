6ff2f030330e73bf006d9ba94d9f52d50c2111eb
Jesufemi <oladapofemmy@gmail.com>
2023-11-28 17:33:48 +0100

    task 8 done

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for ch in str:
+        if ord(ch) >= 97 and ord(ch) <= 122:
+            new_name = ord(ch) - 32
+        else:
+            new_name = ord(ch)
+        print("{}".format(chr(new_name)), end="")
