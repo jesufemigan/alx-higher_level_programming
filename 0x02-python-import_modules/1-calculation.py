b3c85a5014241aeb07f0b3b490d3686811f78880
Jesufemi <oladapofemmy@gmail.com>
2023-11-30 15:51:05 +0100

    fix typo 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,7 +4,7 @@ def calculator():
     a = 10
     b = 5
     print("{} + {} = {:d}".format(a, b, add(a, b)))
-    print("{} - {} = {:d}".format(a, b, add(a, b)))
+    print("{} - {} = {:d}".format(a, b, sub(a, b)))
     print("{} * {} = {:d}".format(a, b, mul(a, b)))
     print("{} / {} = {:d}".format(a, b, div(a, b)))
 
