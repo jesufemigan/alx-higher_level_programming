4e47d1f2b38d7c0dc3bf34dc1398c3baf02e5968
Jesufemi <oladapofemmy@gmail.com>
2023-12-02 23:06:02 +0100

    task 6 done

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if not my_list:
+        return None
     my_list.reverse()
     [print("{:d}".format(x)) for x in my_list]
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
+        if len(matrix[i]) == 0:
+            print()
         for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end=" ")
-        print()
+            print("{:d}".format(matrix[i][j]), end="\n" if j is len(matrix[i]) - 1
+                  else " ")
