942395bd9466824127ecf34ca038737eb49c8973
Jesufemi <oladapofemmy@gmail.com>
2023-12-02 22:45:59 +0100

    task 2 fix

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return None
-    for i in range(len(my_list)):
-        if i == idx:
-            my_list.remove(my_list[i])
-            my_list.insert(i, element)
+    my_list[idx] = element;
     return my_list
