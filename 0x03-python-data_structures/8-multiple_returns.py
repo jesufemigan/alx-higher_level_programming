6455c3609f86dc792bfd21e9df6acf0a1a0f2ed8
Jesufemi <oladapofemmy@gmail.com>
2023-12-03 17:00:17 +0100

    fix 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0 or sentence == "":
-        return (None, 0)
-    return (len(sentence), sentence[0])
+    first_char = sentence[0] if len(sentence) > 0 else None
+    return len(sentence), first_char
