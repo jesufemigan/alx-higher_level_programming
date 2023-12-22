6ee281da2fe6bc03393f8efc2181921eec9be177
Jesufemi <oladapofemmy@gmail.com>
2023-12-22 21:37:50 +0100

    fix 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -19,7 +19,7 @@ class Square:
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
-            rasie ValueError('size must be >= 0')
+            raise ValueError('size must be >= 0')
         self.__size = size
 
     def area(self):
