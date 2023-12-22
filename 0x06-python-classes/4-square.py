a0799b64945d51c9966add8555e40481c0f2cb6f
Jesufemi <oladapofemmy@gmail.com>
2023-12-22 21:54:07 +0100

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""A square module"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size=0):
+        """Constructor
+
+        Args:
+            size: a side of square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Property for the length of a side of this square.
+
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than zero
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
