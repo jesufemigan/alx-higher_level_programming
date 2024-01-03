6987bd9ef6bbd93738151ec072d1d01862b789f5
Jesufemi <oladapofemmy@gmail.com>
2024-01-03 15:42:08 +0100

    add documentation to task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+"""
+A module that adds two numbers
+numbers must be either float or integer
+"""
+
 
 def add_integer(a, b=98):
+    """
+        adds two numbers
+        the two numbers must be either float or integer
+        the two numbers will be converted to int before adding
+        Returns: a + b
+    """
     if not type(a) in [int, float]:
         raise TypeError('a must be an integer')
     if not type(b) in [int, float]:
