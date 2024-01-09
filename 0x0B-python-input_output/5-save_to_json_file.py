a6f00019e81639e8dda38cdf304ae0bb4db3df9e
Jesufemi <oladapofemmy@gmail.com>
2024-01-09 23:24:08 +0100

    task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''saves to JSON'''
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    '''function to save to JSON file'''
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
