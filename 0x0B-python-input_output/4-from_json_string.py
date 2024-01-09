2a11ac2fbfed0f514ae4a7a3836506f26de89fa6
Jesufemi <oladapofemmy@gmail.com>
2024-01-09 23:21:15 +0100

    task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''a module to onvert from JSON to string'''
+
+
+import json
+
+
+def from_json_string(my_str):
+    '''fucntion from JSON to sttirng'''
+    return json.loads(my_str)
