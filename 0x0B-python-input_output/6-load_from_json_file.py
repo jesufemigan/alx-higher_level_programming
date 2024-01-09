085709c2fd0b579e8913779e62d667c31d56955e
Jesufemi <oladapofemmy@gmail.com>
2024-01-09 23:26:54 +0100

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''module that loads into JSON'''
+
+
+import json
+
+
+def load_from_json_file(filename):
+    '''function that loads from JSON'''
+    with open(filename, "r", encoding="utf-8") as f:
+        json.load(f)
