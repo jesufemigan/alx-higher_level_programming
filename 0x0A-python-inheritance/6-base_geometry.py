318f49034015e4214d5bb41a5a0677393cc8403c
Jesufemi <oladapofemmy@gmail.com>
2024-01-09 19:04:16 +0100

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A module for base geometry"""
+
+
+class BaseGeometry:
+    """A custom class basegeometry"""
+    def area(self):
+        """area method to raise exception"""
+        raise Exception("area() is not implemented")
