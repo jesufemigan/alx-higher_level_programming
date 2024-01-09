faf444178318bebabf0fe3f65b9ba839a5d1edaa
Jesufemi <oladapofemmy@gmail.com>
2024-01-09 15:21:12 +0100

    fix 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,10 +6,6 @@ Module with a class MyList that inherits from list
 
 class MyList(list):
     """A class that inherits from list class"""
-    def __init__(self):
-        """Constructor for class MyList"""
-        super().__init__()
-
     def print_sorted(self):
         """A function that prints a sorted list"""
         print(sorted(self))
