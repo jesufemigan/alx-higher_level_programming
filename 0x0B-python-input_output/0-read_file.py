c671a3ff02ba87659302eba2c9792aa90cae60c6
Jesufemi <oladapofemmy@gmail.com>
2024-01-09 22:46:58 +0100

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''A module that reads file'''
+
+
+def read_file(filename=""):
+    '''reads a file'''
+    with open(filename, mode="r", encoding="utf-8") as myFile:
+        myFile.read()
