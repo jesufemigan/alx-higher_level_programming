bd02aa11d80cf95012beefc5c65c9acb96f26716
Jesufemi <oladapofemmy@gmail.com>
2024-01-15 20:44:42 +0100

    15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 '''A module for the base class'''
 
 
+from json import dumps, loads
+
+
 class Base:
     '''A class Base which is the base for all other classes'''
     __nb_objects = 0
@@ -13,3 +16,11 @@ class Base:
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''returns the JSON string representation'''
+        if not list_dictionaries or list_dictionaries is None:
+            return "[]"
+        return dumps(list_dictionaries)
+
