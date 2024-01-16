40fdd3b4e7b26a0e31b5cb3d71bce8ad553753fe
Jesufemi <oladapofemmy@gmail.com>
2024-01-16 14:32:32 +0100

    update test for base class

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -8,18 +8,53 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
     '''A test for the base class'''
-    @classmethod
-    def setUpClass(cls):
-        '''Function to initialise the class of Base'''
-        cls.a = Base()
-        cls.b = Base(90)
-        cls.c = Base()
+    def setUp(self):
+        '''Imports module, instantiates class'''
+        Base._Base__nb_objects = 0
+        pass
 
-    def test_base_class(self):
-        '''Check if base class initialises correctly'''
-        self.assertEqual(self.a.id, 1)
-        self.assertEqual(self.b.id, 90)
-        self.assertEqual(self.c.id, 2)
+    def test_A_nb_objects_private(self):
+        '''tests if nb_objects is private class attributes'''
+        self.assertTrue(hasattr(Base, "_Base__nb_objects"))
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_B_nb_objects_initialised(self):
+        '''tests if nb_objects initializes to zero'''
+        self.assertEqual(getattr(Base, "_Base__nb_objects"), 0)
+
+    def test_C_instantiaition(self):
+        '''Tests Base() instantiation'''
+        b = Base()
+        self.assertEqual(str(type(b)), "<class 'models.base.Base'>")
+        self.assertEqual(b.__dict__, {"id": 1})
+        self.assertEqual(b.id, 1)
+
+    def test_D_constructor(self):
+        '''tests constructor signature'''
+        with self.assertRaises(TypeError) as e:
+            Base.__init__()
+        msg = "Base.__init__() missing 1 required positional argument: 'self'"
+        self.assertEqual(str(e.exception), msg)
+
+    def test_D_constructor_args_2(self):
+        '''test constructor signature with 2 notself args'''
+        with self.assertRaises(TypeError) as e:
+            Base.__init__(self, 1, 2)
+        msg = "Base.__init__() takes from 1 to 2 positional arguments but 3 \
+were given"
+        self.assertEqual(str(e.exception), msg)
+
+    def test_E_consecutive_ids(self):
+        '''tests consective ids'''
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id + 1, b2.id)
+
+    def test_F_id_synced(self):
+        '''tests sync between class and instance id'''
+        b = Base()
+        self.assertEqual(getattr(Base, "_Base__nb_objects"), b.id)
+
+    def test_G_custom_id_int(self):
+        '''tests custom int id'''
+        b = Base(98)
+        self.assertEqual(b.id, 98)
