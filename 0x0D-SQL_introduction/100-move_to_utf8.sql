5add9981d1b0916651cbfb269994eba2b05d6f67
Jesufemi <oladapofemmy@gmail.com>
2024-02-13 09:31:15 -0800

    no chnage

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,5 @@
 -- converts database to utf-8 format
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE hbtn_0c_0.first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table DROP COLUMN `name`;
+ALTER TABLE hbtn_0c_0.first_table ADD COLUMN `name` VARCHAR(256) COLLATE utf8mb4_unicode_ci;
