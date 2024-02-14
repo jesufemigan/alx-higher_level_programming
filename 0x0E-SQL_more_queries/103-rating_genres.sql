67e7d2701738b8f9c9d6e8eb1dfff883c3e631d8
Jesufemi <oladapofemmy@gmail.com>
2024-02-14 23:43:41 +0100

    task 103

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,10 @@
+--  lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+SELECT g.name, SUM(r.rate) as rating FROM tv_genres g
+LEFT JOIN tv_show_genres t
+ON g.id = t.genre_id
+LEFT JOIN tv_shows s
+ON s.id = t.show_id
+LEFT JOIN tv_show_ratings r
+ON s.id = r.show_id
+GROUP BY g.name
+ORDER BY rating DESC;
