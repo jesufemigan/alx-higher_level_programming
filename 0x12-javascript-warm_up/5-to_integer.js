883462ba0e6ba5e2eb85a1929b57668941a44d5c
Jesufemi <oladapofemmy@gmail.com>
2024-03-11 21:34:27 +0100

    fix 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const firstArg = process.argv[2];
 if (isNaN(firstArg)) {
   console.log('Not a number');
 } else {
-  console.log('My Number: ' + firstArg);
+  console.log('My number: ' + firstArg);
 }
