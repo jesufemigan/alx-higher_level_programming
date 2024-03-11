8532e0aec7e00d78570c4fa5ffc484d1d44333a7
Jesufemi <oladapofemmy@gmail.com>
2024-03-11 21:59:37 +0100

    task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const size = process.argv[2];
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = size; i > 0; i--) {
+    let square = '';
+    for (let j = size; j > 0; j--) {
+      square += 'X';
+    }
+    console.log(square);
+  }
+}
