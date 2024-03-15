f030afdf41c71e86ef2636e9e904d2cc64b5469b
Jesufemi <oladapofemmy@gmail.com>
2024-03-15 14:09:46 +0100

    task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+'''lists all in cities'''
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("""SELECT c.name
+                FROM cities c INNER JOIN states s
+                ON c.state_id = s.id
+                WHERE s.name LIKE %s
+                GROUP BY c.name
+                ORDER BY c.id""", [sys.argv[4]])
+
+    rows = cur.fetchall()
+    tmp = list(row[0] for row in rows)
+    print(*tmp, sep=", ")
+    cur.close()
+    db.close()
