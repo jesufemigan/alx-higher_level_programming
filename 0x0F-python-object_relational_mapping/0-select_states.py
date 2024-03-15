a73237b891dcf1dbb96a1136dca5b3174ada2854
Jesufemi <oladapofemmy@gmail.com>
2024-03-15 11:52:54 +0100

    task 0 done

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''lists all states from the database'''
+
+
+import MySQLdb
+import sys
+
+db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2],
+                     db=sys.argv[3])
+cur = db.cursor()
+
+cur.execute('SELECT * FROM states ORDER BY id')
+rows = cur.fetchall()
+
+for row in rows:
+    print(f"{row}")
+cur.close()
+db.close()
