1ee5ce3cbdceb4420038091caafbf092ffad37eb
Jesufemi <oladapofemmy@gmail.com>
2024-03-28 14:56:02 +0100

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''A module that fetches froma url'''
+
+if __name__ == '__main__':
+    import urllib.request
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        content_type = content.__class__
+        utf8 = content.decode("utf-8")
+
+
+print(f'Body response:\n\t- type: {content_type}\n\t- content: \
+{content}\n\t- utf8 content: {utf8}')
