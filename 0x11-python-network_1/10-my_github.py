64b03c26a7df53af58593e1d09675b93ca8f2f5e
Jesufemi <oladapofemmy@gmail.com>
2024-03-28 21:43:57 +0100

    fix 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     # token = "github_pat_11ALOLOSA0kivU4eXnI1jQ_CfzP7IDwqDtj5P2mT7\
 # tpbWZYl7CZbd9TV9K35J4NSqXPH3ZQVIDM7qaeADP"
     headers = {"Accept": "application/vnd.github+json",
-               "Authorization": sys.argv[2],
+               "Authorization": f"Bearer {sys.argv[2]}",
                "X-Github-Api-Version": "2022-11-28"}
     r = requests.get(url, headers=headers)
     user = r.json()
