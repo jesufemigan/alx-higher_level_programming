5431a69b889deb2a7cfae9792863badea5fd0a62
Jesufemi <oladapofemmy@gmail.com>
2024-03-28 15:16:05 +0100

    task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''sends POST request to an URL with email parameter'''
+
+if __name__ == '__main__':
+    import urllib.request
+    import urllib.parse
+    import sys
+
+    data = urllib.parse.urlencode({'email': sys.argv[2]})
+    url = sys.argv[1]
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        result = response.read().decode("utf-8")
+    print(result)
