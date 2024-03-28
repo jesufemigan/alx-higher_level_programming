9f9b157641e38a4df17f099b3fbbe6cbd180fefd
Jesufemi <oladapofemmy@gmail.com>
2024-03-28 21:37:13 +0100

    task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''use Github API'''
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = "https://api.github.com/users/" + sys.argv[1]
+    token = "github_pat_11ALOLOSA0kivU4eXnI1jQ_CfzP7IDwqDtj5P2mT7\
+tpbWZYl7CZbd9TV9K35J4NSqXPH3ZQVIDM7qaeADP"
+    headers = {"Accept": "application/vnd.github+json",
+               "Authorization": token, "X-Github-Api-Version": "2022-11-28"}
+    r = requests.get(url, headers=headers)
+    user = r.json()
+    print(user['id'])
