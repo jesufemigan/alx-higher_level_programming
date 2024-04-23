4506167a1237ca09401674e1e3dc3ca7a0675923
Jesufemi <oladapofemmy@gmail.com>
2024-04-23 14:47:32 +0100

    f3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,9 +6,9 @@ const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
 request(url, (err, res, data) => {
-	if (err) {
-		console.log(err);
-	}
-	let json = JSON.parse(data)
-	console.log(json.title);
+  if (err) {
+    console.log(err);
+  }
+  const json = JSON.parse(data);
+  console.log(json.title);
 });
